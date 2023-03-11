7307878aa42a43e61a2404229381292d9989099f
SergeyPavluchenko <102330212+SergeyPavluchenko@users.noreply.github.com>
2023-03-11 22:19:54 +0000

    Deploying to gh-pages from @ SergeyPavluchenko/goit-react-hw-08-phonebook@ad38a3eb3d7990e5c16119b0e3e788e8786942a9 🚀

---
+++ static/js/980.5ae4bd4a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[980],{7980:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r,a=n(168),c=(n(6444).ZP.h1(r||(r=(0,a.Z)(["\n  font-weight: 1100;\n  font-size: 78px;\n  text-align: center;\n"]))),n(184),function(){})}}]);
+//# sourceMappingURL=980.5ae4bd4a.chunk.js.map
